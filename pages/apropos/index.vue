59ff81cb3cd6c74d80e54293bb89c77f4252545b
pcweb <contact@pcweb-conseils.com>
2020-12-16 19:14:24 +0100

    commit

--- pages/apropos/index.vue
+++ pages/apropos/index.vue
@@ -1,6 +1,5 @@
 <template>
     <section>
         <h1 class="maclasse">Hello world.</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque laboriosam doloremque nihil temporibus adipisci?</p>
     </section>
 </template>
