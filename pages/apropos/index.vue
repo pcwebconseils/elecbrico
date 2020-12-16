cbfe7fe1ef1ed271592028ee7c90e3749a944444
pcweb <contact@pcweb-conseils.com>
2020-12-16 19:06:59 +0100

    rajout paragraphe

--- pages/apropos/index.vue
+++ pages/apropos/index.vue
@@ -1,5 +1,6 @@
 <template>
     <section>
-        <h1>Hello world.</h1>
+        <h1 class="maclasse">Hello world.</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque laboriosam doloremque nihil temporibus adipisci?</p>
     </section>
 </template>
